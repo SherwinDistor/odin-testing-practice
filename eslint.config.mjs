6f6f1b0f2f2d0f8b2752333ddf1dd28ca7a20990
sherwindistor <182566354+SherwinDistor@users.noreply.github.com>
2025-01-30 16:02:56 -0600

    Add jest eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import { expect, jest, test } from '@jest/globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     { languageOptions: { globals: { ...globals.node, ...globals.browser } } },
     pluginJs.configs.recommended,
     eslintConfigPrettier,
+    { expect, jest, test },
 ];
